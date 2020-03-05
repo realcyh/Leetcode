5758751eef96db7de3a689dcfecf6dd11069e699
YH CHEN <realcyh@outlook.com>
2020-03-05 17:22:52 -0500

    lc105 Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/array/MyThread.java
@@ -0,0 +1,17 @@
+package array;
+
+public class MyThread {
+    public static void main(String args[]) {
+        boolean b = true;
+        if((b == true) || doStuff(true)) {
+            System.out.println("A");
+        }
+    }
+    public static boolean doStuff(boolean bool) {
+        if(bool == true) {
+            System.out.println("B");
+        }
+        System.out.println("C");
+        return true;
+    }
+}
